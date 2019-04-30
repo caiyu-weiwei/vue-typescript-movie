8908d5aa1ec314692354ea9d5de721d6edcec075
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-04-30 14:49:03 +0800

    code update

--- movie/src/App.vue
+++ movie/src/App.vue
@@ -8,6 +8,16 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Header from '@/components/Header.vue'
+@Component({
+  components: {
+    Header
+  }
+})
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
