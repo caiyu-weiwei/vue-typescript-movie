945d2fd37578cfe4188718da0c064a0287dad1e2
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-05-06 17:08:26 +0800

    用interface来定义数据类型

---
+++ movie/src/store/interface.ts
@@ -0,0 +1,33 @@
+export interface movie {
+  name: string;
+  rating: rating;
+  genres: any[];
+  title: string;
+  casts: start[];
+  collect_count: number;
+  original_title: string;
+  subtype: string;
+  directors: any[];
+  year: string;
+  images: any[];
+  alt: string;
+  id: string;
+};
+
+export interface rating {
+  max: number;
+  average: string;
+  stars: number;
+  min: number;
+}
+
+export interface start {
+  alt: string;
+  avatars: any[];
+  name: string;
+  id: string;
+}
+
+export interface state {
+  movieList?: movie[];
+}
